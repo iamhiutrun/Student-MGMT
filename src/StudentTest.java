6ebaa5e1a8ed37d5bc49e685361997eafc624f03
HiuTrun <62102580+HiuTrun@users.noreply.github.com>
2021-03-19 17:27:50 +0700

    Provide 7 functions in Question 3

--- src/StudentTest.java
+++ src/StudentTest.java
@@ -1,5 +1,7 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,7 +10,6 @@ public class StudentTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		Student student = new Student();
 		Menu(sc);
 		sc.close();
 
@@ -16,7 +17,6 @@ public class StudentTest {
 	
 	public static void Menu(Scanner sc) {
 		List<Student> studentList = new ArrayList<>();
-		int studentNumber = 0;
 		boolean cont = true;
 		do {
 			System.out.println("----------------MENU-----------------");
@@ -34,9 +34,8 @@ public class StudentTest {
 				cont = false;
 				break;
 			case 1:
-				System.out.println("Nhap so sinh vien n = ");
+				System.out.print("Nhap so sinh vien n = ");
 				int n = sc.nextInt();
-				studentNumber += n;
 				for(int i = 1;i<=n;i++) {
 					System.out.println("Nhap thong tin sinh vien "+i);
 					sc.nextLine();
@@ -48,12 +47,57 @@ public class StudentTest {
 				break;
 			case 2:
 				System.out.println("Hien thi sinh vien: ");
-				for(int i = 1; i<=studentNumber;i++) {
-					studentList.get(i).showInfo();
+				for(Student student : studentList) {
+					System.out.println(student.showInfo());
 				}
 				break;
 			case 3: 
+				Collections.sort(studentList,new Comparator<Student>() {
+					@Override
+					public int compare(Student o1, Student o2) {
+						return o2.getAverage().compareTo(o1.getAverage());
+					}
+					
+				});
+				System.out.println("SINH VIEN CO DIEM CAO NHAT: ");
+				System.out.println(studentList.get(0).showInfo());
+				System.out.println("SINH VIEN CO DIEM THAP NHAT: ");
+				System.out.println(studentList.get(studentList.size()-1).showInfo());
+				break;
+			case 4:
+				System.out.println("TIM KIEM SINH VIEN THEO MA SINH VIEN");
+				System.out.print("Mhap MSSV = ");
+				String mssv = sc.next();
+				boolean status = false;
+				for (Student student : studentList) {
+					if(student.getStudentID().contains(mssv)) {
+						status = true;
+						System.out.println("Tim thanh cong");
+						System.out.println(student.showInfo());
+					}
+				}
+				if(status == false) System.out.println("Khong tim thay sinh vien nao!");
+				break;
 				
+			case 5:
+				System.out.println("HIEN THI SINH VIEN THEO THU TU TU [A-Z]");
+				Collections.sort(studentList, new Comparator<Student>() {
+					@Override
+					public int compare(Student o1, Student o2) {
+						return o2.getName().compareTo(o1.getName());
+					}
+				});
+				for (Student student : studentList) {
+					System.out.println(student.showInfo());
+				}
+				break;
+			case 6:
+				System.out.println("SINH VIEN DAT HOC BONG:");
+				for (Student student : studentList) {
+					if(student.isScholarship()) {
+						System.out.println(student.showInfo());
+					}
+				}
 				break;
 			default:
 				break;
